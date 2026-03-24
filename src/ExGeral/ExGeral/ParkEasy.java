c0603b2a749fa47e9a9bece2727850c9bdeb9749
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-24 11:37:23 -0300

    Exercicio parcialmente finalizado

--- src/ExGeral/ExGeral/ParkEasy.java
+++ src/ExGeral/ExGeral/ParkEasy.java
@@ -7,6 +7,8 @@ public class ParkEasy {
     static Scanner inp = new Scanner(System.in);
     static Veiculos[] veiculos = new Veiculos[10];
     static int nVeiculo;
+    static int nRegistro;
+    static Hora[] registro = new Hora[20];
 
     public static void main(String[] args) {
 
@@ -37,13 +39,65 @@ public class ParkEasy {
 
     }
 
+    private static Veiculos pesquisar() {
+        String placa;
+        System.out.printf("Placa do veículo:    ");
+        placa = inp.next().toUpperCase();
+
+        for (int i = 0; i < nVeiculo; i++) {
+            if (veiculos[i].placa.equals(placa)) {
+                return veiculos[i];
+            }
+        }
+        System.out.println("[(?) Veículo não encontrado]");
+        return null;
+    }
+
     private static void entradaVeiculo() {
+        String nome;
+        long cpf;
+        String marca, modelo, placa;
+        String horaEntrada;
+
+        Veiculos veiculoEncontrado = pesquisar();
+
+        if (veiculoEncontrado == null) {
+            System.out.printf("Nome do proprietário:    ");
+            nome = inp.next();
+            System.out.printf("Cpf: ");
+            cpf = inp.nextLong();
+
+            System.out.printf("Marca: ");
+            marca = inp.next();
+            System.out.printf("Modelo: ");
+            modelo = inp.next();
+            System.out.printf("Placa: ");
+            placa = inp.next().toUpperCase();
+
+            Proprietario proprietario = new Proprietario(nome, cpf);
+            veiculos[nVeiculo] = new Veiculos(marca, modelo, placa, proprietario);
+
+            nVeiculo++;
+        } else {
+            System.out.printf("Hora de entrada:    ");
+            horaEntrada = inp.next();
+
+            registro[nRegistro] = new Hora();
+        }
+
     }
 
     private static void saidaVeiculo() {
     }
 
     private static void imprimeVeiculo() {
+
+        for (int i = 0; i < nRegistro; i++) {
+            System.out.println(registro[i].veiculos.placa);
+            System.out.println(registro[i].veiculos.marca);
+            System.out.println(registro[i].veiculos.modelo);
+
+        }
     }
 
     private static void imprimeReceita() {
